a8fba3360a8f6abbb287b0fec47a4efc3116a4ca
baidoosik <qoentlr37@naver.com>
2017-08-06 03:21:56 +0900

    layout 변경 및 accounts앱 추가

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url,include
+from django.contrib.auth.views import login,logout
+from . import views
+
+urlpatterns={
+    url(r'^signup/$',views.signup,name='signup'),
+    url(r'^login/$',login,name='login'),
+    url(r'^logout/$',login,name='logout'),
+    url(r'^profile/$',views.profile,name='profile'),
+
+}
