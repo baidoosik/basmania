443659d464f876f7096463f2f42d2a3406a46ea1
baidoosik <qoentlr37@naver.com>
2017-08-06 04:11:36 +0900

    home 화면에 경기수,플레이어수,팀수전달하기

--- basmania/settings.py
+++ basmania/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'club',
     'accounts',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- club/views.py
+++ club/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from .models import Team,Player,Match
 # Create your views here.
 def home(request):
-    return render(request,'club/home.html')
+    team_count = Team.objects.all().count()
+    player_count = Player.objects.all().count()
+    match_count = Match.objects.all().count()
+
+    return render(request,'club/home.html',{
+        'team_count':team_count,
+        'player_count':player_count,
+        'match_count':match_count,
+    })
 
