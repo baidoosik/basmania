054f86a2b346b8663e0038f3411373995ca2a3c5
baidoosik <qoentlr37@naver.com>
2017-08-06 05:57:00 +0900

    팀 정렬 및 선수 검색화면

--- club/views.py
+++ club/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
-from .models import Team,Player,Match
+from .models import Team,Player,Match,Performance
 # Create your views here.
+
 def home(request):
-    team_count = Team.objects.all().count()
-    player_count = Player.objects.all().count()
+    teams = Team.objects.all()
+    team_count = teams.count()
+    player= Player.objects.all()
+    player_count=player.count()
     match_count = Match.objects.all().count()
 
-    return render(request,'club/home.html',{
-        'team_count':team_count,
-        'player_count':player_count,
-        'match_count':match_count,
-    })
+    q = request.GET.get('q', '')
+
+    if q:
+        player= player.filter(name__icontains=q)
+
+        return render(request,'club/home.html',{
+            'team_count':team_count,
+            'player_count':player_count,
+            'match_count':match_count,
+            'players':player,
+        })
+    else:
+        return render(request,'club/home.html',{
+            'team_count':team_count,
+            'player_count':player_count,
+            'match_count':match_count,
+            'teams': teams,
+        })
+
 
